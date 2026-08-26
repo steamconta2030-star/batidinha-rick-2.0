e9c7ec4d5f3cd7ab99249438c0a0bf972cec14a7
TechLabHouse <steamconta2030@gmail.com>
2026-08-26 14:31:36 -0300

    style: activate Batidinha do Rick global brand theme

--- batidinha-rick-2.0-atualizado/src/main.tsx
+++ batidinha-rick-2.0-atualizado/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./visual-polish.css";
+import "./brand-theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
